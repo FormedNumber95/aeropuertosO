99e2562ddb5c2ce20aff814cdbc118fe0e46c523
FormedNumber95 <aketza200510@gmail.com>
2024-10-27 13:01:53 +0100

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/aeropuertosO/ProductoLabel.java
@@ -0,0 +1,17 @@
+package es.aketzagonzalez.aeropuertosO;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ProductoLabel {
+
+    @FXML
+    private ImageView imgProducto;
+
+    @FXML
+    private Label lblNombreProducto;
+
+    @FXML
+    private Label lblStockProducto;
+
+}
