79957189501000aca58bf8b739031dcbcc302014
FormedNumber95 <aketza200510@gmail.com>
2024-10-27 14:05:00 +0100

    acabado ejercicio
    acabado el ejercico y comprobado que el setStock funciona correctamente
    27/10/2024

--- src/main/java/es/aketzagonzalez/aeropuertosO/ProductoLabel.java
+++ src/main/java/es/aketzagonzalez/aeropuertosO/ProductoLabel.java
@@ -1,17 +1,78 @@
 package es.aketzagonzalez.aeropuertosO;
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
-public class ProductoLabel {
+/**
+ * The Class ProductoLabel.
+ */
+public class ProductoLabel extends GridPane {
+	
+	/** The lbl nombre producto. */
+	@FXML
+	protected Label lblNombreProducto;
+	
+	/** The lbl stock. */
+	@FXML
+	protected Label lblStock;
+	
+	/** The img producto. */
+	@FXML
+	private ImageView imgProducto;
 
-    @FXML
-    private ImageView imgProducto;
+	/**
+	 * Instantiates a new producto label.
+	 */
+	public ProductoLabel() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/componenteTrigo.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		try {
+			fxmlLoader.load();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
 
-    @FXML
-    private Label lblNombreProducto;
+	/**
+	 * Sets the stock.
+	 *
+	 * @param cantidad the new stock
+	 */
+	public void setStock(double cantidad) {
+		String style;
+		if (cantidad > 100) {
+			style = "verde";
+		} else if (cantidad > 20) {
+			style = "azul";
+		} else {
+			style = "rojo";
+		}
+		lblStock.setText(cantidad + "");
+		this.getStyleClass().clear();
+		this.getStyleClass().add(style);
+	}
 
-    @FXML
-    private Label lblStockProducto;
+	/**
+	 * Sets the image.
+	 *
+	 * @param imagen the new image
+	 */
+	public void setImage(Image imagen) {
+		imgProducto.setImage(imagen);
+	}
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param nombre the new name
+	 */
+	public void setName(String nombre) {
+		lblNombreProducto.setText(nombre + "");
+	}
 }
